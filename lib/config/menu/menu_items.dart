eea57b3b6de68fb5a9eaabbf578a86bc8a5e6749
Alessandro Hidaldo <alessandrohidalgo8@gmail.com>
2023-05-29 11:55:16 -0400

    inicio de la aplicacion

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,32 @@
+/*import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subtitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem(
+      {required this.title,
+      required this.subtitle,
+      required this.link,
+      required this.icon});
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+      title: "primer SQLite",
+      subtitle: "ensayo de como usarlo",
+      link: "/SQLite",
+      icon: Icons.data_array_outlined),
+  MenuItem(
+      title: "segundo SQLite",
+      subtitle: "seguimos",
+      link: "/2SQLite",
+      icon: Icons.access_alarm_outlined),
+  MenuItem(
+      title: "tercer SQLite",
+      subtitle: "otra prueba",
+      link: "/3SQLite",
+      icon: Icons.edit)
+];*/
---
+++ lib/screens/screen.dart
@@ -0,0 +1,1 @@
+export 'package:uniprocess_app/screens/home_screen.dart';
