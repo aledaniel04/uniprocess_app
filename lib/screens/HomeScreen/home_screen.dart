9f8da478cd5f9cee7dfc60f4cf6a33f998e10127
Alessandro Hidaldo <alessandrohidalgo8@gmail.com>
2023-06-07 15:08:08 -0400

    agregue nuevas funciones al HomeScreen

--- lib/screens/HomeScreen/home_screen.dart
+++ lib/screens/HomeScreen/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:uniprocess_app/screens/HomeScreen/add_update_note_screen.dart';
+import 'package:intl/intl.dart';
 import 'package:uniprocess_app/screens/HomeScreen/db_helper_note.dart';
 import 'package:uniprocess_app/screens/HomeScreen/model_note.dart';
 import 'package:uniprocess_app/screens/screen.dart';
@@ -17,6 +17,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   DBHelperNote? dbHelperNote;
   late Future<List<NoteModel>> dataList;
+  final _fromkey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -27,6 +28,185 @@ class _HomeScreenState extends State<HomeScreen> {
 
   loadData() async {
     dataList = dbHelperNote!.getDataList();
+  }
+
+  Future<void> _showAddNoteDialog() async {
+    final titleController = TextEditingController();
+    final descController = TextEditingController();
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Agregar Nota'),
+          content: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              key: _fromkey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    controller: titleController,
+                    decoration: const InputDecoration(
+                      labelText: 'Título',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Este campo es requerido';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    maxLines: null,
+                    minLines: 5,
+                    keyboardType: TextInputType.multiline,
+                    controller: descController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Descripción',
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Este campo es requerido';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_fromkey.currentState!.validate()) {
+                  final title = titleController.text;
+                  final description = descController.text;
+                  _addNote(title, description);
+                  Navigator.of(context).pop();
+                }
+              },
+              child: const Text('Guardar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _addNote(String title, String description) async {
+    final note = NoteModel(
+      title: title,
+      desc: description,
+      dateandtime: DateFormat('yMd').add_jm().format(DateTime.now()).toString(),
+    );
+
+    await dbHelperNote!.insert(note);
+    setState(() {
+      dataList = dbHelperNote!.getDataList();
+    });
+  }
+
+  Future<void> _showUpdateNoteDialog(int noteId, String currentTitle,
+      String currentDesc, String currentDT) async {
+    final titleController = TextEditingController(text: currentTitle);
+    final descController = TextEditingController(text: currentDesc);
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Actualizar Nota'),
+          content: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              key: _fromkey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    controller: titleController,
+                    decoration: const InputDecoration(
+                      labelText: 'Título',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Este campo es requerido';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    maxLines: null,
+                    minLines: 5,
+                    keyboardType: TextInputType.multiline,
+                    controller: descController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Descripción',
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Este campo es requerido';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_fromkey.currentState!.validate()) {
+                  final updatedTitle = titleController.text;
+                  final updatedDesc = descController.text;
+                  _updateNote(noteId, updatedTitle, updatedDesc);
+                  Navigator.of(context).pop();
+                }
+              },
+              child: const Text('Actualizar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _updateNote(
+      int noteId, String updatedTitle, String updatedDesc) async {
+    final updatedNote = NoteModel(
+      id: noteId,
+      title: updatedTitle,
+      desc: updatedDesc,
+      dateandtime: DateFormat('yMd').add_jm().format(DateTime.now()).toString(),
+    );
+
+    await dbHelperNote!.update(updatedNote);
+    setState(() {
+      dataList = dbHelperNote!.getDataList();
+    });
   }
 
   @override
@@ -127,6 +307,34 @@ class _HomeScreenState extends State<HomeScreen> {
                                 color: Colors.white,
                               ),
                             ),
+                            confirmDismiss: (DismissDirection direction) async {
+                              return await showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text('Eliminar Nota'),
+                                    content: const Text(
+                                        '¿Estás seguro de eliminar esta nota?'),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop(
+                                              false); // Cancelar eliminación
+                                        },
+                                        child: const Text('Cancelar'),
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop(
+                                              true); // Confirmar eliminación
+                                        },
+                                        child: const Text('Eliminar'),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
                             onDismissed: (DismissDirection direction) {
                               setState(() {
                                 dbHelperNote!.delete(noteId);
@@ -140,7 +348,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                   color: Color.fromARGB(93, 87, 76, 144),
                                   boxShadow: const [
                                     BoxShadow(
-                                      color: Colors.black12,
+                                      color: Color.fromARGB(245, 245, 245, 245),
                                       blurRadius: 4,
                                       spreadRadius: 1,
                                     )
@@ -182,24 +390,15 @@ class _HomeScreenState extends State<HomeScreen> {
                                         ),
                                         InkWell(
                                           onTap: () {
-                                            Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        AddUpdateNote(
-                                                          noteid: noteId,
-                                                          noteTitle: noteTitle,
-                                                          noteDesc: noteDesc,
-                                                          noteDT: noteDT,
-                                                          update: true,
-                                                        )));
+                                            _showUpdateNoteDialog(noteId,
+                                                noteTitle, noteDesc, noteDT);
                                           },
                                           child: const Icon(
                                             Icons.edit_note_outlined,
                                             size: 30,
                                             color: Color.fromARGB(255, 0, 0, 0),
                                           ),
-                                        )
+                                        ),
                                       ],
                                     ),
                                   )
@@ -213,21 +412,11 @@ class _HomeScreenState extends State<HomeScreen> {
                   }))
         ],
       ),
-      floatingActionButton: Align(
-        alignment: Alignment.bottomLeft,
-        child: Padding(
-          padding: const EdgeInsets.only(left: 300.0, bottom: 35.0),
-          child: FloatingActionButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => AddUpdateNote()));
-            },
-            backgroundColor: Color.fromARGB(255, 114, 103, 141),
-            child: const Icon(Icons.add),
-          ),
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddNoteDialog,
+        backgroundColor: const Color.fromARGB(255, 114, 103, 141),
+        child: const Icon(Icons.add),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
     );
   }
 }
