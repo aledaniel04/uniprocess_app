70841db03a622644c3146486a07e4bb938a00ab6
Alessandro Hidaldo <alessandrohidalgo8@gmail.com>
2023-06-09 04:02:29 -0400

    creacion del plan de evaluacion

--- lib/screens/evaluationPlanScreen/evaluation_plan_screen.dart
+++ lib/screens/evaluationPlanScreen/evaluation_plan_screen.dart
@@ -1,18 +1,371 @@
 import 'package:flutter/material.dart';
+import 'package:uniprocess_app/screens/evaluationPlanScreen/db_helper_evaluationPlan.dart';
+import 'package:intl/intl.dart';
 
 class EvaluationPlanScreen extends StatefulWidget {
-  const EvaluationPlanScreen({super.key});
+  final String period;
+  final String career;
+  final String subject;
+  final String section;
+  final String semester;
+
+  const EvaluationPlanScreen({
+    Key? key,
+    required this.period,
+    required this.career,
+    required this.subject,
+    required this.section,
+    required this.semester,
+  }) : super(key: key);
 
   @override
   State<EvaluationPlanScreen> createState() => _EvaluationPlanScreenState();
 }
 
 class _EvaluationPlanScreenState extends State<EvaluationPlanScreen> {
+  List<Map<String, dynamic>> _allData = [];
+  final _formKey = GlobalKey<FormState>();
+  final DateFormat _dateFormatter = DateFormat("MMM dd, yyyy");
+  DateTime _date = DateTime.now();
+
+  bool _isLoading = true;
+
+  // This function is used to fetch all data from the database
+  void _refreshData() async {
+    final data = await DBHelperEvaluationPlan.getsingleDataStudenstList(
+      widget.period,
+      widget.career,
+      widget.subject,
+      widget.section,
+      widget.semester,
+    );
+    setState(() {
+      _allData = data;
+      _isLoading = false;
+    });
+  }
+
+  final TextEditingController _objectiveController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _assessmentController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshData();
+    _dateController.text =
+        _dateFormatter.format(_date); // Loading the diary when the app starts
+  }
+
+  @override
+  void dispose() {
+    _objectiveController.dispose();
+    _contentController.dispose();
+    _dateController.dispose();
+    _assessmentController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _handleDatePicker() async {
+    final DateTime? date = await showDatePicker(
+      context: context,
+      initialDate: _date,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (date != null && date != _date) {
+      setState(() {
+        _date = date;
+      });
+      _dateController.text = _dateFormatter.format(date);
+    }
+  }
+
+  void _showDialogEvaluation(int? id) async {
+    bool isNewCareer = id == null;
+    String dialogTitle = isNewCareer ? 'Nuevo Unidad' : 'Actualizar Unidad';
+    if (id != null) {
+      final existingData =
+          _allData.firstWhere((element) => element['id'] == id);
+      _objectiveController.text = existingData['objective'];
+      _contentController.text = existingData['content'];
+      _dateController.text = existingData['date'];
+      _assessmentController.text = existingData['assessment'];
+    }
+
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Center(
+          child: Text(
+            dialogTitle,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        content: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: _objectiveController,
+                  decoration: InputDecoration(
+                    hintText: 'Objetivo de la Unidad',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(40.0),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Este campo es requerido';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  maxLines: null,
+                  minLines: 5,
+                  keyboardType: TextInputType.multiline,
+                  controller: _contentController,
+                  decoration: InputDecoration(
+                    hintText: 'Contenido',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(40.0),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Este campo es requerido';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  readOnly: true,
+                  controller: _dateController,
+                  style: const TextStyle(fontSize: 18.0),
+                  onTap: _handleDatePicker,
+                  decoration: InputDecoration(
+                    labelText: "fecha",
+                    labelStyle: const TextStyle(fontSize: 18.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: _assessmentController,
+                  decoration: InputDecoration(
+                    hintText: 'instrumento de evaluacion',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(40.0),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Este campo es requerido';
+                    }
+                    return null;
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          TextButton(
+            child: Text('Cancelar'),
+            onPressed: () {
+              Navigator.of(context).pop();
+              _objectiveController.text = '';
+              _contentController.text = '';
+              _dateController.text = '';
+              _assessmentController.text = '';
+            },
+          ),
+          ElevatedButton(
+            child: Text(id == null ? 'Agregar Estudiante' : 'Actualizar'),
+            onPressed: () async {
+              if (_formKey.currentState!.validate()) {
+                if (id == null) {
+                  await _addData();
+                } else {
+                  await _updateData(id);
+                }
+
+                _objectiveController.text = '';
+                _contentController.text = '';
+                _dateController.text = '';
+                _assessmentController.text = '';
+
+                Navigator.of(context).pop();
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showDeleteConfirmationDialog(int id) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Confirmar eliminación'),
+        content:
+            const Text('¿Estás seguro de que quieres eliminar esta Unidad?'),
+        actions: [
+          TextButton(
+            child: const Text('Cancelar'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          ElevatedButton(
+            child: const Text('Eliminar'),
+            onPressed: () {
+              Navigator.of(context).pop();
+              _deleteItem(id);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _addData() async {
+    await DBHelperEvaluationPlan.createData(
+      widget.period,
+      widget.career,
+      widget.subject,
+      widget.section,
+      widget.semester,
+      _objectiveController.text,
+      _contentController.text,
+      _dateController.text,
+      _assessmentController.text,
+    );
+    _refreshData();
+  }
+
+  Future<void> _updateData(int id) async {
+    await DBHelperEvaluationPlan.updateData(
+      id,
+      widget.period,
+      widget.career,
+      widget.subject,
+      widget.section,
+      widget.semester,
+      _objectiveController.text,
+      _contentController.text,
+      _dateController.text,
+      _assessmentController.text,
+    );
+    _refreshData();
+  }
+
+  void _deleteItem(int id) async {
+    await DBHelperEvaluationPlan.deleteData(id);
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        backgroundColor: Colors.redAccent,
+        content: Text('eliminaste un estudiantes'),
+      ),
+    );
+    _refreshData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 237, 237, 242),
       appBar: AppBar(
-        title: const Text("Plan de Evaluacion"),
+        backgroundColor: const Color.fromARGB(255, 192, 199, 212),
+        centerTitle: true,
+        title: const Text(
+          "Plan de evaluacion",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "${widget.period} > ${widget.career} > ${widget.subject} > ${widget.section} > ${widget.semester}",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+            ),
+          ),
+          Expanded(
+            child: _isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ListView.builder(
+                    itemCount: _allData.length,
+                    itemBuilder: (context, index) => Card(
+                      color: Colors.lime[200],
+                      margin: const EdgeInsets.all(15),
+                      child: InkWell(
+                        onLongPress: () => _showDeleteConfirmationDialog(
+                            _allData[index]['id']),
+                        child: ListTile(
+                          onTap: () =>
+                              _showDialogEvaluation(_allData[index]['id']),
+                          title: Flexible(
+                            child: Text(
+                              _allData[index]['objective'],
+                              style: const TextStyle(fontSize: 20),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          subtitle: Flexible(
+                            child: Text(
+                              _allData[index]["content"],
+                              style: const TextStyle(fontSize: 10),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          trailing: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              /*Text(
+                                _dateFormatter.format(
+                                  DateTime.parse(_allData[index]['date']),
+                                ),
+                                style: const TextStyle(fontSize: 10),
+                                overflow: TextOverflow.ellipsis,
+                              ),*/
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Flexible(
+                                child: Text(
+                                  _allData[index]['assessment'],
+                                  style: const TextStyle(fontSize: 20),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _showDialogEvaluation(null),
       ),
     );
   }
